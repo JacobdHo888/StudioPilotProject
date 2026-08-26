5d02a089695453816cde1eaaa9cb31d331518a03
JacobdHo <148382652+JacobdHo888@users.noreply.github.com>
2026-08-26 14:54:49 -0700

    Added restart button for progress status.

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'CDwmesxiYvZlZQSs159sw6a4CepsY-mT',
+            'X-App-Proxy': 'aKcVamwD3-n-2MtVqExm2f4hzB_ZmXQC',
           },
           body: JSON.stringify(requestDetails),
         };
