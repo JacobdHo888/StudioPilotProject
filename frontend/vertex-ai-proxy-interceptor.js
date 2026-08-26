0a8835f01bfef3af6c0c9de502f62ac0bd6a4a55
JacobdHo <148382652+JacobdHo888@users.noreply.github.com>
2026-08-26 13:13:15 -0700

    Revamped the user interface and redesigned the scope

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'GaUFzZqmgWQUoiD3lHBSiiyRuQXPVM7X',
+            'X-App-Proxy': 'dvn2qxdSMEwQjrWGkxtuWRFsYrZm7__G',
           },
           body: JSON.stringify(requestDetails),
         };
